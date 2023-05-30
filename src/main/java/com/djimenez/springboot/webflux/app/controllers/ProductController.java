70b02d813ee4f18e3f8b442b610fcf8169bf6cbc
Diego Jimenez <diegourieljd@gmail.com>
2023-05-30 17:38:31 -0600

    upload image added

--- src/main/java/com/djimenez/springboot/webflux/app/controllers/ProductController.java
+++ src/main/java/com/djimenez/springboot/webflux/app/controllers/ProductController.java
@@ -1,11 +1,15 @@
 package com.djimenez.springboot.webflux.app.controllers;
 
+import java.io.File;
 import java.net.URI;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.djimenez.springboot.webflux.app.models.documents.Product;
@@ -27,6 +32,9 @@ public class ProductController {
 	
 	@Autowired
 	private ProductService productService;
+	
+	@Value("${config.uploads.path}")
+	String path;
 	
 	@GetMapping
 	public Mono<ResponseEntity<Flux<Product>>> findAll() {
@@ -48,6 +56,33 @@ public class ProductController {
 		return productService.save(product).map(p -> ResponseEntity.created(URI.create("api/product/".concat(p.getId())))
 				.contentType(MediaType.APPLICATION_JSON)
 				.body(p));
+	} 
+	
+	@PostMapping("/upload/{id}")
+	public Mono<ResponseEntity<Product>> upload(@PathVariable String id, @RequestPart FilePart file) {
+		return productService.findById(id).flatMap(p -> {
+			p.setPhoto(UUID.randomUUID().toString() + "-" + file.filename()
+				.replace(" ", "")
+				.replace(":", "")
+				.replace("//", ""));
+			
+			return file.transferTo(new File(path + p.getPhoto())).then(productService.save(p));
+		}).map(p -> ResponseEntity.ok(p))
+		.defaultIfEmpty(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping("/v2")
+	public Mono<ResponseEntity<Product>> create(Product product, @RequestPart FilePart file) {
+		
+		product.setPhoto(UUID.randomUUID().toString() + "-" + file.filename()
+			.replace(" ", "")
+			.replace(":", "")
+			.replace("//", ""));
+		
+		return file.transferTo(new File(path + product.getPhoto())).then(productService.save(product))
+			.map(p -> ResponseEntity.created(URI.create("api/product/".concat(p.getId())))
+			.contentType(MediaType.APPLICATION_JSON)
+			.body(p));
 	} 
 	
 	@PutMapping("/{id}")
